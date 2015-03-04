277ddeb76ecf90b5e76ea83a2de33b0d31cf109a
Willy Burgos <willy.brgs@gmail.com>
2015-03-04 18:42:00 +0100

    Bug length et signe

--- ft_formatnbr.c
+++ ft_formatnbr.c
@@ -6,7 +6,7 @@
 /*   By: wburgos <wburgos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/04 18:03:26 by wburgos           #+#    #+#             */
-/*   Updated: 2015/03/04 18:31:15 by wburgos          ###   ########.fr       */
+/*   Updated: 2015/03/04 18:37:09 by wburgos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,14 @@ static int	put_zeroes(int minw, int prec, int len, int opts)
 	return (i);
 }
 
-static int put_sign(intmax_t n, int opts)
+static int put_sign(intmax_t n, int opts, int *tmp)
 {
 	char	sign;
 
 	sign = 0;
 	if (n < 0)
 		sign = '-';
-	else if (n > 0)
+	else if (n >= 0)
 	{
 		if (opts & PLUS)
 			sign = '+';
@@ -61,6 +61,7 @@ static int put_sign(intmax_t n, int opts)
 		ft_putchar(sign);
 	if (sign == 0 || sign == '-')
 		return (0);
+	(*tmp)++;
 	return (1);
 }
 
@@ -90,7 +91,7 @@ int		ft_formatnbr(intmax_t n, int opts, int minw, int prec)
 	tmp = len;
 	if (!(opts & MINUS) && !(opts & ZERO))
 		len += put_spaces(minw, prec, tmp);
-	len += put_sign(n, opts);
+	len += put_sign(n, opts, &tmp);
 	if ((!(opts & MINUS) && opts & ZERO) || opts & PRECISION)
 		len += put_zeroes(minw, prec, tmp, opts);
 	ft_putunbr((n < 0) ? (-n) : (n));
